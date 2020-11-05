d37dc25b8f4c5e22f429898a46c2c14c4f72d48b
lapalugunarathne@gmail.com <lapalugunarathne@gmail.com>
2020-11-05 18:58:58 +0530

    Add BusInfo model

---
+++ TranspotationTicketBooking/Models/BusInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TranspotationTicketBooking.Models
+{
+    public class BusInfo
+    {
+        public long BusNo { get; set; }
+        public string DriverName { get; set; }
+        public long  DriverNo{ get; set; }
+        public string CondName { get; set; }
+        public long CondNo { get; set; }
+        public long MaxSeats { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Verified { get; set; }
+    
+    }
+}
