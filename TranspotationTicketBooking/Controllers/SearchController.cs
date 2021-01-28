683cd1fd8ed8e07e163450a9fe30c3a8775cdd98
harithlakmith <hlakmith@gmail.com>
2021-01-28 16:59:45 +0530

    add search controller

---
+++ TranspotationTicketBooking/Controllers/SearchController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TranspotationTicketBooking.Models;
+using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
+using System.Web;
+
+namespace TranspotationTicketBooking.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        private readonly TicketBookingDBContext _context;
+
+        public SearchController(TicketBookingDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: search/SearchTicket?date=#&from=#&to=#+
+
+
+        [HttpGet("{SearchTicket}")]
+        public async Task<ActionResult<IEnumerable<Session>>> GetSessions(DateTime date, string from, string to )
+        {
+
+            //var sessions = await _context.Session.Where(s => s.Date == date).ToListAsync();
+           var sessions = (from s in _context.Session.Where(s => s.Date == date)
+                            select new Session()
+                            {
+                                RId = s.RId
+                            }).ToList();
+           
+            
+            return sessions;
+
+        }
+
+
+
+
+
+
+
+        // PUT: api/Session/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSession(long id, Session session)
+        {
+            if (id != session.SId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(session).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SessionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Session
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Session>> PostSession(Session session)
+        {
+            _context.Session.Add(session);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSession", new { id = session.SId }, session);
+        }
+
+        // DELETE: api/Session/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Session>> DeleteSession(long id)
+        {
+            var session = await _context.Session.FindAsync(id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+
+            _context.Session.Remove(session);
+            await _context.SaveChangesAsync();
+
+            return session;
+        }
+
+        private bool SessionExists(long id)
+        {
+            return _context.Session.Any(e => e.SId == id);
+        }
+    }
+}
